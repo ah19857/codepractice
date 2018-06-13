5c07fecbaf3c2184484a7b206aed115ca5d0e65c
AH <ah2950@gmail.com>
2018-06-12 20:00:24 -0500

    global variables

---
+++ globalvar.js
@@ -0,0 +1,19 @@
+var myGlobal; // Declare your variable here
+var oopsGlobal;
+
+function fun1() {
+  myGlobal = 10; // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
